8e41c1a027079f61fe5eed7b96da1fa9e68a6268
ngocgia <vodangngocgia@gmail.com>
2022-11-11 22:01:26 +0700

    add luckyFish

--- main.js
+++ main.js
@@ -165,6 +165,20 @@ window.addEventListener('load', function(){
         }
     }
 
+    class LuckyFish extends Enemy {
+        constructor(game){
+            super(game);
+            this.width = 99;
+            this.height = 95;
+            this.y = Math.random() * (this.game.height * 0.9 - this.height);
+            this.image = document.getElementById('lucky');
+            this.frameY = Math.floor(Math.random() * 2);
+            this.lives = 3;
+            this.score = 15;
+            this.type = 'lucky';
+        }
+    }
+
     class Layer{
         constructor(game, image, speedModifier) {
             this.game = game;
@@ -330,8 +344,9 @@ window.addEventListener('load', function(){
         }
         addEnemy(){
             const ramdomize = Math.random();
-            if(ramdomize < 0.5)  this.enemies.push(new Angler1(this));
-            else this.enemies.push(new Angler2(this));
+            if(ramdomize < 0.3)  this.enemies.push(new Angler1(this));
+            else if (ramdomize < 0.6) this.enemies.push(new Angler2(this));
+            else this.enemies.push(new LuckyFish(this));
             console.log(this.enemies);
         }
         checkCollison(rect1, rect2){
